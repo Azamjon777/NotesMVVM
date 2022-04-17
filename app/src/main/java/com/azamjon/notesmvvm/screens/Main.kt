e20ac735d5a70b0124ebe725628485ac04bd1443
Azamjon777 <84564553+Azamjon777@users.noreply.github.com>
2022-04-17 20:16:28 +0500

    Big App with MVVM, Firebase, Room

---
+++ app/src/main/java/com/azamjon/notesmvvm/screens/Main.kt
@@ -0,0 +1,10 @@
+package com.azamjon.notesmvvm.screens
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+
+
+@Composable
+fun Main(navController: NavHostController) {
+
+}
---
+++ app/src/main/java/com/azamjon/notesmvvm/screens/Start.kt
@@ -0,0 +1,11 @@
+package com.azamjon.notesmvvm.screens
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+
+
+@Composable
+fun Start(navController: NavHostController) {
+    Text(text = "Notes App\nStart Activity")
+}
